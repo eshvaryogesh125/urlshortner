08826f3c1ecc6711ba66cc6bb7b4cd17892c4ee0
yogesh <eshvaryogesh45@gmail.com>
2026-03-29 15:08:23 +0530

    added mock cache redis

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ from sqlalchemy.orm import Session
 import models
 import schemas
 import utils
+import time
 from database import engine, SessionLocal
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+CACHE = {}
 
 @app.get("/health")
 def health_check():
@@ -53,11 +55,33 @@ def create_short_url(request: schemas.URLRequest, db: Session = Depends(get_db))
 @app.get("/{short_code}")
 def redirect(short_code: str, db: Session = Depends(get_db)):
 
-    url = db.query(models.URL).filter(
-        models.URL.short_code == short_code
-    ).first()
+    original_url = get_cache(short_code)
+    print(f"Cache hit for {short_code}: {original_url}" if original_url else f"Cache miss for {short_code}")
+    if not original_url:
+        url = db.query(models.URL).filter(
+            models.URL.short_code == short_code
+        ).first()
 
-    if not url:
-        raise HTTPException(status_code=404, detail="URL not found")
+        if not url:
+            raise HTTPException(status_code=404, detail="URL not found")
+        
+        original_url = url.original_url if url else None
+        set_cache(short_code, original_url)
+        print(f"URL found in DB for {short_code}: {original_url}" if url else f"No URL found in DB for {short_code}")
 
-    return RedirectResponse(url.original_url, status_code=301)
+
+    return RedirectResponse(original_url, status_code=301)
+
+def get_cache(short_code):
+    print(f"Checking cache for {short_code}, Current cache state: {CACHE}")
+    if short_code in CACHE:
+        original_url, expiry = CACHE[short_code]
+        if time.time() > expiry:
+            del CACHE[short_code]
+            return None
+        return original_url
+    return None
+
+def set_cache(short_code, original_url, ttl=600):
+    expiry = time.time() + ttl
+    CACHE[short_code] = (original_url, expiry)
